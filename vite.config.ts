5223a3c658255f32956f0b40bec02f68697264e8
paula-zeman <p.zeman@posteo.de>
2025-01-26 22:04:52 +0100

    adjust config again

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/invitation/",
   plugins: [react()],
+  base: "invitation/",
   server: {
     port: 3000,
   },
