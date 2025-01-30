c2c33b7b9d96cbafea0b90d5efac435fe44824be
Nils Dammenhayn <nilsdam@aol.com>
2025-01-30 21:12:17 +0100

    test BrowserRouter instead of HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import "./App.css";
-import { HashRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import Routes from "./Router/Routes";
 import AuthProvider from "./Context/AuthProvider";
 
 function App() {
   return (
-    <HashRouter basename={import.meta.env.VITE_APP_BASE_NAME || ""}>
+    <BrowserRouter basename={import.meta.env.VITE_APP_BASE_NAME || ""}>
       <AuthProvider>
         <Routes />
       </AuthProvider>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
